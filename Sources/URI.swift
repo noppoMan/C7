19f452537746dd421f3a25c8397b73c6ccd8e7be
Paulo Faria <paulo.faria.rl@gmail.com>
2016-03-20 23:38:29 -0300

    add initializers

--- Sources/URI.swift
+++ Sources/URI.swift
@@ -2,11 +2,21 @@ public struct URI {
     public struct UserInfo {
         public var username: String
         public var password: String
+
+        public init(username: String, password: String) {
+            self.username = username
+            self.password = password
+        }
     }
 
     public struct Query {
         public var key: String
         public var value: String?
+
+        public init(key: String, value: String? = nil) {
+            self.key = key
+            self.value = value
+        }
     }
 
     public var scheme: String?
