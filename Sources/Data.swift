245e887adf9147b40f10e9f28d64ec8bcc28aa67
Paulo Faria <paulo.faria.rl@gmail.com>
2016-03-26 12:46:22 -0300

    add convenience functions and properties to data

--- Sources/Data.swift
+++ Sources/Data.swift
@@ -31,7 +31,137 @@ public struct Data {
 }
 
 extension Data {
-    public init() {
-        self.bytes = []
+    public init(_ string: String) {
+        self.init([Byte](string.utf8))
     }
+}
+
+extension Data: RangeReplaceableCollection {
+    public init() {
+        self.init([])
+    }
+
+    public init(repeating repeatedValue: Byte, count: Int) {
+        self.init([Byte](repeating: repeatedValue, count: count))
+    }
+
+    public mutating func replaceSubrange<C : Collection where C.Iterator.Element == Byte>(subRange: Range<Int>, with newElements: C) {
+        self.bytes.replaceSubrange(subRange, with: newElements)
+    }
+
+    public mutating func reserveCapacity(n: Int) {
+        self.bytes.reserveCapacity(n)
+    }
+
+    public init<S : Sequence where S.Iterator.Element == Byte>(_ elements: S) {
+        self.init([Byte](elements))
+    }
+
+    public mutating func append(x: Byte) {
+        self.bytes.append(x)
+    }
+
+    public mutating func append<S : Sequence where S.Iterator.Element == Byte>(contentsOf newElements: S) {
+        self.bytes.append(contentsOf: newElements)
+    }
+
+
+    public mutating func insert(newElement: Byte, at i: Int) {
+        self.bytes.insert(newElement, at: i)
+    }
+
+    public mutating func insert<S : Collection where S.Iterator.Element == Byte>(contentsOf newElements: S, at i: Int) {
+        self.bytes.insert(contentsOf: newElements, at: i)
+
+    }
+
+    public mutating func remove(at i: Int) -> Byte {
+        return self.bytes.remove(at: i)
+    }
+
+    public mutating func removeFirst() -> Byte {
+        return self.bytes.removeFirst()
+    }
+
+    public mutating func removeFirst(n: Int) {
+        self.bytes.removeFirst(n)
+    }
+
+    public mutating func removeSubrange(bounds: Range<Int>) {
+        self.bytes.removeSubrange(bounds)
+    }
+
+    public mutating func removeAll(keepingCapacity keepCapacity: Bool) {
+        self.bytes.removeAll(keepingCapacity: keepCapacity)
+    }
+}
+
+extension Data: MutableCollection {
+    public func makeIterator() -> IndexingIterator<[Byte]> {
+        return bytes.makeIterator()
+    }
+
+    public var startIndex: Int {
+        return bytes.startIndex
+    }
+
+    public var endIndex: Int {
+        return bytes.endIndex
+    }
+
+    public var count: Int {
+        return bytes.count
+    }
+
+    public subscript(index: Int) -> Byte {
+        get {
+            return bytes[index]
+        }
+
+        set(value) {
+            bytes[index] = value
+        }
+    }
+
+    public subscript (bounds: Range<Int>) -> ArraySlice<Byte> {
+        get {
+            return bytes[bounds]
+        }
+
+        set(slice) {
+            bytes[bounds] = slice
+        }
+    }
+}
+
+extension Data: NilLiteralConvertible {
+    public init(nilLiteral: Void) {
+        self.init([])
+    }
+}
+
+extension Data: ArrayLiteralConvertible {
+    public init(arrayLiteral bytes: Byte...) {
+        self.init(bytes)
+    }
+}
+
+extension Data: StringLiteralConvertible {
+    public init(stringLiteral string: String) {
+        self.init(string)
+    }
+
+    public init(extendedGraphemeClusterLiteral string: String){
+        self.init(string)
+    }
+
+    public init(unicodeScalarLiteral string: String){
+        self.init(string)
+    }
+}
+
+extension Data: Equatable {}
+
+public func ==(lhs: Data, rhs: Data) -> Bool {
+    return lhs.bytes == rhs.bytes
 }
