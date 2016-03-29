54d2c5b04de9ed9646821f2a9209de3ba9b494d6
Tanner Nelson <tanner@bluebite.com>
2016-03-29 16:22:15 -0400

    add tests

---
+++ Tests/ExampleTests.swift
@@ -0,0 +1,16 @@
+import XCTest
+@testable import C7
+
+class ExampleTests: XCTestCase {
+    static var allTests : [(String, ConfigTests -> () throws -> Void)] {
+        return [
+           ("testReality", testReality),
+        ]
+    }
+
+	func testReality() {
+		let config = self.config(.Development)
+		XCTAssert(2 + 2 == 4, "Something is severely wrong here.")
+	}
+
+}
