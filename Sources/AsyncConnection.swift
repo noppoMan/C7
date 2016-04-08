2bc1d7a02930ff8b6d36558b5b327b89a5d0c9de
Paulo Faria <paulo.faria.rl@gmail.com>
2016-04-08 13:39:35 -0300

    add URIConnection

--- Sources/AsyncConnection.swift
+++ Sources/AsyncConnection.swift
@@ -1,5 +1,3 @@
 public protocol AsyncConnection: AsyncStream {
-    init(to uri: URI) throws
-    var uri: URI { get }
     func open(timingOut deadline: Int64, result: (Void throws -> AsyncConnection) -> Void) throws
 }
--- Sources/Connection.swift
+++ Sources/Connection.swift
@@ -1,5 +1,3 @@
 public protocol Connection: Stream {
-    init(to uri: URI) throws
-    var uri: URI { get }
     func open(timingOut deadline: Int64) throws
 }
