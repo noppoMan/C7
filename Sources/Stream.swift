02787c8c1f453f4aede9e3544e52c863a90178b1
Oleg Dreyman <dreymonde@me.com>
2016-04-14 18:28:54 +0300

    result to completion

--- Sources/Stream.swift
+++ Sources/Stream.swift
@@ -12,12 +12,12 @@ public protocol ReceivingStream: Closable, Receiving {}
 public protocol Stream: SendingStream, ReceivingStream {}
 
 extension Sending {
-    public func send(_ data: Data, timingOut deadline: Double, result: (Void throws -> Void) -> Void) {
-        result { try self.send(data, timingOut: deadline) }
+    public func send(_ data: Data, timingOut deadline: Double, completion: (Void throws -> Void) -> Void) {
+        completion { try self.send(data, timingOut: deadline) }
     }
 
-    public func flush(timingOut deadline: Double, result: (Void throws -> Void) -> Void) {
-        result { try self.flush(timingOut: deadline) }
+    public func flush(timingOut deadline: Double, completion: (Void throws -> Void) -> Void) {
+        completion { try self.flush(timingOut: deadline) }
     }
 }
 
@@ -32,8 +32,8 @@ extension Sending {
 }
 
 extension Receiving {
-    public func receive(upTo byteCount: Int, timingOut deadline: Double, result: (Void throws -> Data) -> Void) {
-        result { try self.receive(upTo: byteCount, timingOut: deadline) }
+    public func receive(upTo byteCount: Int, timingOut deadline: Double, completion: (Void throws -> Data) -> Void) {
+        completion { try self.receive(upTo: byteCount, timingOut: deadline) }
     }
 }
 
